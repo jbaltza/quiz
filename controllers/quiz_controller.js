11b9f770b18248f55071234b4e762e8b3ce7a683
jbaltza <jbaltza1@yahoo.es>
2015-08-03 22:28:31 +0200

    Indice Tematico

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -60,7 +60,7 @@ exports.create = function (req, res) {
 				res.render('quizes/new', {quiz: quiz, errors: err.errors});
 			} else {
 				quiz // save: guarda en la DB los campos pregunta y respuesta de quiz
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta", "respuesta","tema"]})
 				.then(function () { res.redirect('/quizes'); });
 			}
 		}
@@ -78,8 +78,9 @@ exports.edit = function (req, res) {
 
 // PUT /quizes/:id
 exports.update = function (req, res) {
-	req.quiz.pregunta = req.body.quiz.pregunta;
-	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.pregunta 	= req.body.quiz.pregunta;
+	req.quiz.respuesta 	= req.body.quiz.respuesta;
+	req.quiz.tema 		= req.body.quiz.tema;
 	
 	
 	req.quiz
@@ -90,7 +91,7 @@ exports.update = function (req, res) {
 				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 			} else {
 				req.quiz // save: guarda en la DB los campos pregunta y respuesta de quiz
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta", "respuesta", "tema"]})
 				.then(function () { res.redirect('/quizes'); });
 			}
 		}
