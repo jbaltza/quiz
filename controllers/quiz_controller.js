058be594cf710b7a63c2becbe1bafb793ac84de5
jbaltza <jbaltza1@yahoo.es>
2015-08-13 22:38:05 +0200

    P2P Modulo7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,9 +17,18 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function (req, res) {
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index.ejs', {quizes: quizes, errors: []});
-	}).catch(function(error) { next(error);});	
+	if (req.query.search) {
+		var search = "%" + req.query.search + "%";
+		models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes) {
+			res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+		}).catch(function(error) { next(error);});
+	} else {
+		models.Quiz.findAll().then(function(quizes) {
+			res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+		}).catch(function(error) { next(error);});	
+	}
+	
+	
 };
 
 // GET /quizes/:id
